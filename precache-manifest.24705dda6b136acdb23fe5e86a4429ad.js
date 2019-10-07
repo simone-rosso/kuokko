0c8e0dca38f32e6d9369a2ee94c1ad3176a8db9f
Simone <srosso@mariabarcelona.com>
2019-10-07 10:31:19 +0200

    Updates

---
+++ precache-manifest.24705dda6b136acdb23fe5e86a4429ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603b2d3d87ac9d8d3cb9ebfa5adb3a84",
+    "url": "/kuocco-dev/index.html"
+  },
+  {
+    "revision": "c04e46a0801340fb73da",
+    "url": "/kuocco-dev/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c6760fd5482147ede720",
+    "url": "/kuocco-dev/static/js/2.0d8a3085.chunk.js"
+  },
+  {
+    "revision": "c04e46a0801340fb73da",
+    "url": "/kuocco-dev/static/js/main.63ec3401.chunk.js"
+  },
+  {
+    "revision": "a4fe9b69de487c93824a",
+    "url": "/kuocco-dev/static/js/runtime-main.d2dcc850.js"
+  }
+]);
